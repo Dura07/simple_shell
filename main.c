5be6e932e7553690f4983524b594d9c63cd95b97
Dura07 <oyedejioluwasheye2013@gmail.com>
2023-11-15 01:21:09 -0800

    main.c

--- main.c
+++ main.c
@@ -8,24 +8,24 @@
  */
 int main(void)
 {
-        char command[MAX_COMMAND_LENGTH];
+	char command[MAX_COMMAND_LENGTH];
 
-        while (1)
-        {
-                display_prompt();
+	while (1)
+	{
+		display_prompt();
 
-                if (fgets(command, sizeof(command), stdin) == NULL)
-                {
-                        if (isatty(STDIN_FILENO))
-                        {
-                                printf("\n");
-                        }
-                        break;
-                }
+		if (fgets(command, sizeof(command), stdin) == NULL)
+		{
+			if (isatty(STDIN_FILENO))
+			{
+				printf("\n");
+			}
+			break;
+		}
 
-                command[strcspn(command, "\n")] = '\0';
+		command[strcspn(command, "\n")] = '\0';
 
-                execute_command(command);
-        }
-        return (0);
+		execute_command(command);
+	}
+	return (0);
 }
