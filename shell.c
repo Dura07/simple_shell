18b9edac534694d27e4d6cc910553b419e9e5171
Dura07 <oyedejioluwasheye2013@gmail.com>
2023-11-15 01:50:47 -0800

    shell.c

--- shell.c
+++ shell.c
@@ -8,16 +8,16 @@
  */
 static void execute_child_process(char *command)
 {
-        char *args[2];
+	char *args[2];
 
-        args[0] = command;
-        args[1] = NULL;
+	args[0] = command;
+	args[1] = NULL;
 
-        if (execve(command, args, environ) == -1)
-        {
-                perror(command);
-                _exit(EXIT_FAILURE);
-        }
+	if (execve(command, args, environ) == -1)
+	{
+		perror(command);
+		_exit(EXIT_FAILURE);
+	}
 }
 
 
@@ -30,11 +30,11 @@ static void execute_child_process(char *command)
 
 void display_prompt(void)
 {
-        if (isatty(STDIN_FILENO))
-        {
-                printf("($) ");
-                fflush(stdout);
-        }
+	if (isatty(STDIN_FILENO))
+	{
+		printf("($) ");
+		fflush(stdout);
+	}
 }
 
 /**
@@ -43,29 +43,29 @@ void display_prompt(void)
  */
 void execute_command(char *command)
 {
-        char *trimmed_command = trim_spaces(command);
+	char *trimmed_command = trim_spaces(command);
 
-        pid_t pid = fork();
+	pid_t pid = fork();
 
-        if (pid == -1)
-        {
-                perror("Fork failed");
-                exit(EXIT_FAILURE);
-        }
-        else if (pid == 0)
-                execute_child_process(trimmed_command);
+	if (pid == -1)
+	{
+		perror("Fork failed");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+		execute_child_process(trimmed_command);
 
-        else
-        {
-                int status;
+	else
+	{
+		int status;
 
-                if (waitpid(pid, &status, 0) == -1)
-                {
-                        perror("Waitpid failed");
-                        exit(EXIT_FAILURE);
-                }
-        }
-        free(trimmed_command);
+		if (waitpid(pid, &status, 0) == -1)
+		{
+			perror("Waitpid failed");
+			exit(EXIT_FAILURE);
+		}
+	}
+	free(trimmed_command);
 }
 /**
  * trim_spaces - Removes leading and trailing spaces from a string.
@@ -74,28 +74,28 @@ void execute_command(char *command)
  */
 char *trim_spaces(char *str)
 {
-        size_t len = strlen(str);
-        size_t start = 0, end = len - 1;
+	size_t len = strlen(str);
+	size_t start = 0, end = len - 1;
 
-        size_t trimmed_len = (end >= start) ? (end - start + 1) : 0;
+	size_t trimmed_len = (end >= start) ? (end - start + 1) : 0;
 
-        char *trimmed = (char *)malloc(trimmed_len + 1);
+	char *trimmed = (char *)malloc(trimmed_len + 1);
 
-        if (!trimmed)
-        {
-                perror("Memory allocation failed");
+	if (!trimmed)
+	{
+		perror("Memory allocation failed");
 
-                exit(EXIT_FAILURE);
-        }
+		exit(EXIT_FAILURE);
+	}
 
-        while (start < len && str[start] == ' ')
-                start++;
+	while (start < len && str[start] == ' ')
+		start++;
 
-        while (end > start && str[end] == ' ')
-                end--;
+	while (end > start && str[end] == ' ')
+		end--;
 
-        strncpy(trimmed, str + start, trimmed_len);
-        trimmed[trimmed_len] = '\0';
+	strncpy(trimmed, str + start, trimmed_len);
+	trimmed[trimmed_len] = '\0';
 
-        return (trimmed);
+	return (trimmed);
 }
